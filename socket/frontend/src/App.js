6339d0b343f2c29d990cc08e94c3e0246329089d
samuelzega <samuel.zegaa@gmail.com>
2022-05-30 20:17:30 +0700

    update socket

---
+++ socket/frontend/src/App.js
@@ -0,0 +1,52 @@
+import { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import io from "socket.io-client";
+import "./App.css";
+
+const socket = io.connect("http://localhost:3010");
+
+function App() {
+  const [message, setMessage] = useState("");
+  const [messages, setMessages] = useState([]);
+
+  function handleTextChange(e) {
+    setMessage(e.target.value);
+    console.log(message);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (!message || message === "") return;
+    console.log("Submitted!");
+    socket.emit("chat message", message);
+  }
+
+  useEffect(() => {
+    socket.on("incoming message", (message) => {
+      setMessages([...messages, message]);
+    });
+  }, [socket, messages]);
+
+  return (
+    <div className="App">
+      <img className="App-logo" src={logo} alt="react logo" />
+      <div className="App-messages">
+        {messages.map((message, index) => (
+          <div className="App-message" key={index}>
+            {message}
+          </div>
+        ))}
+      </div>
+      <form className="App-control" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Message..."
+          onChange={handleTextChange}
+        />
+        <input className="App-button" type="submit" value="Send" />
+      </form>
+    </div>
+  );
+}
+
+export default App;
